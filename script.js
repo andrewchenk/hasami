f7c995e02c576d4184f7a4382f47ce0004df7a30
Andrew Chen <andrewchenk@protonmail.com>
2014-08-05 21:59:52 -0700

    rewrite

--- script.js
+++ script.js
@@ -1,44 +1,56 @@
 $(document).ready(function () {
-    var groupOne = ["う", "く", "ぐ", "す", "つ", "ぬ", "ぶ", "む", "る"];
-    var groupThree = ["来", "きます", "します"];
+        var groupOne = ["う", "く", "ぐ", "す", "つ", "ぬ", "ぶ", "む", "る"];
+        var groupThree = ["来", "きます", "します"];
 
-    //check if in array
-    function isInArray(array, search) {
-        return array.indexOf(search) >= 0;
-    }
+        //check if in array
+        function isInArray(array, search) {
+            return array.indexOf(search) >= 0;
+        }
 
-    //add input to the page
-    function printPage(location, input) {
-        $(location).replaceWith(input);
-    }
+        //add input to the page
+        function printPage(location, input) {
+            $(location).replaceWith(input);
+        }
 
-    var g1 = "Group 1";
-    var g2 = "Group 2";
-    var g3 = "Group 3";
-    var ex = "exception";
+        var g1 = "Group 1";
+        var g2 = "Group 2";
+        var g3 = "Group 3";
+        var ex = "exception";
 
-    //Click the button to get the form value.
-    $("#submit").click(function () {
-        var verb = {};
-        verb.u = $('#enter').val();
-        verb.stem = verb.u.slice(0, -3);
-        printPage("#masu", verb.u);
+        //Click the button to get the form value.
+        $("#submit").click(function () {
+                var verb = {};
+                //put an if check here for masu? LATER
+                verb.group = "";
+                verb.u = $('#enter').val();
+                verb.end = verb.u.slice(0, -1);
+                verb.stem = verb.u.slice(0, -2);
+                verb.masu = "";
+                verb.ta = "";
+                verb.te = "";
 
-        function teConjugate() {
+                printPage("#u", verb.stem);
 
-            //What is the verb we're manipulating?
+                function getVerbGroup() {
+                    if (isInArray(groupOne, verb.end)) {
+                        printPage("#group", g1);
+                    }
 
-            //Check if it's a group three verb and conjugate accordingly.
+                    function teConjugate() {
 
-            //Since group three verbs can possibly intended to be group one verbs, we start a new if block.
+                        //What is the verb we're manipulating?
+
+                        //Check if it's a group three verb and conjugate accordingly.
+
+                        //Since group three verbs can possibly intended to be group one verbs, we start a new if block.
 
 
-            //We'll assume everything else that's not group one is a group two verb.
+                        //We'll assume everything else that's not group one is a group two verb.
 
 
-            //Check if the verb is possibly an exception to the usual group rules. We can add more else ifs for more exceptions and use printType and specify. Use either the group one method (preMasu.change) or the group two function verb.printStemTe accordingly.
+                        //Check if the verb is possibly an exception to the usual group rules. We can add more else ifs for more exceptions and use printType and specify. Use either the group one method (preMasu.change) or the group two function verb.printStemTe accordingly.
 
-        };
-        teConjugate();
-    });
-});
+                    };
+                    teConjugate();
+                });
+        });
