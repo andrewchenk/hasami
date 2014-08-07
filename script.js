9dd622769a66193d60b4c7d4f82e90e6601d55fb
Andrew Chen <andrewchenk@protonmail.com>
2014-08-06 20:35:16 -0700

    group recognition working with dictionary form

--- script.js
+++ script.js
@@ -1,56 +1,62 @@
 $(document).ready(function () {
-        var groupOne = ["う", "く", "ぐ", "す", "つ", "ぬ", "ぶ", "む", "る"];
-        var groupThree = ["来", "きます", "します"];
+    var groupOne = ["う", "く", "ぐ", "す", "つ", "ぬ", "ぶ", "む", "る"];
+    var groupTwo = ["い", "き", "し", "ち", "に", "え", "け", "せ", "て", "ね", "へ", "め", "れ", "ぎ", "じ", "び", "ぴ", "げ", "ぜ", "で", "べ", "ぺ"]
+    var groupThree = ["来", "くる", "する"];
 
-        //check if in array
-        function isInArray(array, search) {
-            return array.indexOf(search) >= 0;
+    //check if in array
+    function isInArray(array, search) {
+        return array.indexOf(search) >= 0;
+    }
+
+    //add input to the page
+    function printPage(id, input) {
+        $("#" + id).replaceWith("<span id = " + id + ">" + input + "</span>");
+    }
+
+    //Click the button to get the form value.
+    $("#submit").click(function () {
+        var verb = {};
+        //put an if check here for masu? LATER
+        verb.group = "";
+        verb.u = $('#enter').val();
+        verb.end = verb.u.slice(-1);
+        verb.endTwo = verb.u.slice(-2, -1);
+        verb.stem = verb.u.slice(0, -2);
+        verb.masu = "";
+        verb.ta = "";
+        verb.te = "";
+
+        printPage("u", verb.u);
+        printPage("stem", verb.stem);
+        printPage("ta", verb.endTwo);
+
+        function getVerbGroup() {
+            if (isInArray(groupThree, verb.u)) {
+                verb.group = "3";
+            } else if (verb.end === "る" && isInArray(groupTwo, verb.endTwo)) {
+                verb.group = "2";
+            } else if (isInArray(groupOne, verb.end)) {
+                verb.group = "1";
+            }
+
+            printPage("group", verb.group);
         }
+        //function teConjugate() {
 
-        //add input to the page
-        function printPage(location, input) {
-            $(location).replaceWith(input);
-        }
+        //What is the verb we're manipulating?
 
-        var g1 = "Group 1";
-        var g2 = "Group 2";
-        var g3 = "Group 3";
-        var ex = "exception";
+        //Check if it's a group three verb and conjugate accordingly.
 
-        //Click the button to get the form value.
-        $("#submit").click(function () {
-                var verb = {};
-                //put an if check here for masu? LATER
-                verb.group = "";
-                verb.u = $('#enter').val();
-                verb.end = verb.u.slice(0, -1);
-                verb.stem = verb.u.slice(0, -2);
-                verb.masu = "";
-                verb.ta = "";
-                verb.te = "";
-
-                printPage("#u", verb.stem);
-
-                function getVerbGroup() {
-                    if (isInArray(groupOne, verb.end)) {
-                        printPage("#group", g1);
-                    }
-
-                    function teConjugate() {
-
-                        //What is the verb we're manipulating?
-
-                        //Check if it's a group three verb and conjugate accordingly.
-
-                        //Since group three verbs can possibly intended to be group one verbs, we start a new if block.
+        //Since group three verbs can possibly intended to be group one verbs, we start a new if block.
 
 
-                        //We'll assume everything else that's not group one is a group two verb.
+        //We'll assume everything else that's not group one is a group two verb.
 
 
-                        //Check if the verb is possibly an exception to the usual group rules. We can add more else ifs for more exceptions and use printType and specify. Use either the group one method (preMasu.change) or the group two function verb.printStemTe accordingly.
+        //Check if the verb is possibly an exception to the usual group rules. We can add more else ifs for more exceptions and use printType and specify. Use either the group one method (preMasu.change) or the group two function verb.printStemTe accordingly.
 
-                    };
-                    teConjugate();
-                });
-        });
+        //};
+        getVerbGroup();
+
+    });
+});
