01d8d4afe9fb3cf3689046740ceb1577ce21b67f
Andrew Chen <andrewchenk@protonmail.com>
2014-08-10 20:59:54 -0700

    te and ta working
    give me a prize

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ $(document).ready(function () {
     hiragana.u = ["う", "く", "す", "つ", "ぬ", "ふ", "む", "ゆ", "る", " ", "ぐ", "ず", "づ", "ぶ", "ぷ"];
     hiragana.e = ["え", "け", "せ", "て", "ね", "へ", "め", " ", "れ", " ", 　"げ", "ぜ", "で", "べ", "ぺ"];
     hiragana.o = ["お", "こ", "そ", "と", "の", "ほ", "も", "よ", "ろ", "を", "ご", "ぞ", "ど", "ぼ", "ぽ"];
-    hiragana.change = function (input, output, initVowel, desiredVowel) {
+    hiragana.teOne = ["み", "に", "び"]
+    hiragana.teTwo = ["い", "ち", "り"]
+    hiragana.change = function (input, initVowel, desiredVowel) {
         var x = hiragana[initVowel].indexOf(input);
         console.log(x);
-        output = hiragana[desiredVowel][x];
-        console.log(output); //just set it to return
+        return hiragana[desiredVowel][x];
+
     }
 
-    var groupOne = ["う", "く", "ぐ", "す", "つ", "ぬ", "ぶ", "む", "る"]
-    var groupTwo = ["い", "き", "し", "ち", "に", "ひ", "み", "り", "え", "け", "せ", "て", "ね", "へ", "め", "れ", "ぎ", "じ", "び", "ぴ", "げ", "ぜ", "で", "べ", "ぺ"]
-    var groupThree = ["来", "くる", "する"];
+    var groupThree = ["くる", "する"];
 
     //check if in array
     function isInArray(array, search) {
@@ -37,79 +37,104 @@ $(document).ready(function () {
         verb.u = $('#enter').val();
         verb.end = verb.u.slice(-1);
         verb.endTwo = verb.u.slice(-2, -1);
-        verb.stem = "";
-        verb.masu = "";
+        verb.withoutEnd = verb.u.slice(0, -1);
+        verb.i = "";
         verb.preMasu = "";
+        verb.masu = "";
         verb.ta = "";
+        verb.taEnd = "";
         verb.te = "";
+        verb.teEnd = "";
+        verb.negative = "";
 
         printPage("u", verb.u);
 
         verb.getGroup = function () {
             if (isInArray(groupThree, verb.u)) {
                 verb.group = "3";
-            } else if (verb.end === "る" && isInArray(groupTwo, verb.endTwo)) {
+            } else if (verb.end === "る" && (isInArray(hiragana.i, verb.endTwo) || isInArray(hiragana.e, verb.endTwo))) {
                 verb.group = "2";
-            } else if (isInArray(groupOne, verb.end)) {
+            } else if (isInArray(hiragana.u, verb.end)) {
                 verb.group = "1";
             }
             printPage("group", verb.group);
         }
 
-        verb.getStem = function () {
+        verb.getI = function () {
             if (verb.group === "1") {
-                /* function checkChangePreMasu(a, b) {
-                    if (verb.end === a) {
-                        verb.preMasu = b;
-                        printPage("ta", verb.preMasu);
-                    }
-                }
-                 checkChangePreMasu("む", "み");
-                checkChangePreMasu("ぬ", "に");
-                checkChangePreMasu("ぶ", "び");
-                checkChangePreMasu("う", "い");
-                checkChangePreMasu("つ", "ち");
-                checkChangePreMasu("る", "り");
-                checkChangePreMasu("く", "き");
-                checkChangePreMasu("ぐ", "ぎ");
-                checkChangePreMasu("す", "し"); */
-
-                hiragana.change(verb["end"], verb["preMasu"], "u", "i");
-                console.log(verb.preMasu);
-                verb.stem = verb.u.slice(0, -1) + verb.preMasu;
+                verb.preMasu = hiragana.change(verb.end, "u", "i");
+                verb.i = verb.u.slice(0, -1) + verb.preMasu;
 
             }
             if (verb.group === "2") {
-                verb.stem = verb.u.slice(0, -1);
+                verb.i = verb.u.slice(0, -1);
             }
 
             if (verb.group === "3") {
 
             }
-            printPage("stem", verb.stem);
+            printPage("i", verb.i);
         }
 
         verb.getMasu = function () {
-            verb.masu = verb.stem + "ます";
+            verb.masu = verb.i + "ます";
             printPage("masu", verb.masu);
         }
+
         verb.getTe = function () {
 
             //Check if it's a group three verb and conjugate accordingly.
             if (verb.group === "3" || verb.group === "2") {
-                verb.te = verb.stem + "て";
+                verb.te = verb.i + "て";
             }
-            //Since group three verbs can possibly intended to be group one verbs, we start a new if block.
-
-
-            //We'll assume everything else that's not group one is a group two verb.
-
-
-            //Check if the verb is possibly an exception to the usual group rules. We can add more else ifs for more exceptions and use printType and specify. Use either the group one method (preMasu.change) or the group two function verb.printStemTe accordingly.
-
+            if (verb.group === "1") {
+                console.log(verb.preMasu);
+                if (isInArray(hiragana.teOne, verb.preMasu)) {
+                    verb.teEnd = "んで";
+                    console.log(verb.teEnd);
+                } else if (isInArray(hiragana.teTwo, verb.preMasu)) {
+                    verb.teEnd = "って";
+                } else if (verb.preMasu = "き") {
+                    verb.teEnd = "いて";
+                } else if (verb.preMasu = "ぎ") {
+                    verb.teEnd = "いで";
+                } else if (verb.preMasu = "し") {
+                    verb.teEnd = "して";
+                }
+                verb.te = verb.withoutEnd + verb.teEnd;
+            }
+            printPage("te", verb.te);
         };
+
+        verb.getTa = function () {
+            if (verb.group === "3" || verb.group === "2") {
+                verb.ta = verb.i + "た";
+            }
+            if (verb.group === "1") {
+                console.log(verb.preMasu);
+                if (isInArray(hiragana.teOne, verb.preMasu)) {
+                    verb.taEnd = "んだ";
+                    console.log(verb.teEnd);
+                } else if (isInArray(hiragana.teTwo, verb.preMasu)) {
+                    verb.taEnd = "った";
+                } else if (verb.preMasu = "き") {
+                    verb.taEnd = "いた";
+                } else if (verb.preMasu = "ぎ") {
+                    verb.taEnd = "いだ";
+                } else if (verb.preMasu = "し") {
+                    verb.taEnd = "した";
+                }
+                verb.ta = verb.withoutEnd + verb.taEnd;
+            }
+            printPage("ta", verb.ta);
+        };
+
+        verb.getNeg
+
         verb.getGroup();
         verb.getStem();
         verb.getMasu();
+        verb.getTe()
+        verb.getTa();
     });
 });
